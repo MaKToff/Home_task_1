da200c02192747f719b4ecee82453c7bc5dadc45
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-10-22 15:32:35 +0400

    Update Task 2.c

--- Task 2.c
+++ Task 2.c
@@ -5,13 +5,13 @@
 
 int main(void)
 {
-    const int bitsInInt = sizeof(int)*8;
-    int number = 0, i = 0;
-    scanf("%d", &number);
-    for (i = 0; i < bitsInInt; ++i)
-    {
-        printf("%d", (number >> bitsInInt - 1) & 1);
-        number = number << 1;
-    }
-    return 0;
+	const int bitsInInt = sizeof(int)*8;
+	int number = 0, i = 0;
+	scanf("%d", &number);
+	for (i = 0; i < bitsInInt; ++i)
+	{
+		printf("%d", (number >> bitsInInt - 1) & 1);
+		number = number << 1;
+	}
+	return 0;
 }
