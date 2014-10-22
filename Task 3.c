133d5195f8c4d15680b2bce04e7bfb96585ba751
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-10-22 23:29:14 +0400

    Update Task 3.c

--- Task 3.c
+++ Task 3.c
@@ -5,7 +5,8 @@
 
 int fitsBits(int number, int word) 
 {
-	int temp = 33 + ~word; //temp = 32 - word
+	const int bitsInInt = sizeof(int) * 8;
+	int temp = bitsInInt + 1 + ~word; //temp = bitsInInt - word
 	int ans = !(((word << temp) >> temp) ^ word);
 	return ans;
 }
