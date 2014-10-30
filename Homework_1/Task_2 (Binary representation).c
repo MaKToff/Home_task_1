5acc3352390ec77a7637ab207231c83035fa15a1
MaKToff <mihail.kita@gmail.com>
2014-10-30 23:24:02 +0300

    Update Task_2 (Binary representation).c

---
+++ Homework_1/Task_2 (Binary representation).c
@@ -0,0 +1,18 @@
+//Output the binary representation of number
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	const int bitsInInt = sizeof(int) * 8;
+	int number = 0, i = 0;
+	scanf("%d", &number);
+	for (i = 0; i < bitsInInt; ++i)
+	{
+		printf("%d", (number >> bitsInInt - 1) & 1);
+		number = number << 1;
+	}
+	return 0;
+}
+
