c94c53d153bc567e96941693e5b93afa2069794a
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-10-31 15:21:26 +0300

    Create Task_1.3 (Float type, method #3).c

---
+++ Homework_2/Task_1.3 (Float type, method #3).c
@@ -0,0 +1,63 @@
+//Takes as input a float number and divides it on sign, exponent and mantissa using bit field. 
+//Then writes this number in other representation
+
+
+#include <stdio.h>
+
+struct floatNumber
+{
+	int sign;
+	int exponent;
+	int mantissa;
+} f;
+
+void print(floatNumber)
+{
+	#define maxNum 255 //It's maximum number for exponent = 2^8 - 1 
+	if (f.sign) f.sign = -1; else f.sign = 1;
+	if (f.exponent == 0 && f.mantissa == 0) printf("Zero\n");
+	else if (f.exponent == maxNum && f.mantissa == 0) 
+	{
+		if (f.sign > 0) printf("+ Infinity\n");
+		else printf("- Infinity\n");
+	}
+	else if (f.exponent == maxNum && f.mantissa != 0) printf("NaN\n");
+	else printf("%d * 2^%d * %f\n", f.sign, f.exponent - 127, 1+((float)f.mantissa)/(1 << 23));
+}
+
+void compute(int bits)
+{
+	int sign = (bits >> 31) & 1;
+	int exponent = (bits >> 23) & ((1 << 8) - 1);
+	int mantissa = bits & ((1 << 23) - 1);
+	f.sign = sign;
+	f.exponent = exponent;
+	f.mantissa = mantissa;
+	print(f);
+}
+
+void method3()
+{
+	union
+	{
+		float floatValue;
+		struct
+		{
+			unsigned m : 23;
+			unsigned e : 8;
+			unsigned s : 1;
+		} bitField;
+	} floatNum;
+
+	scanf("%f", &floatNum.floatValue);
+	f.sign = floatNum.bitField.s;
+	f.exponent = floatNum.bitField.e;
+	f.mantissa = floatNum.bitField.m;
+	print(f);
+}
+
+int main(void)
+{
+	method3();
+	return 0;
+}
