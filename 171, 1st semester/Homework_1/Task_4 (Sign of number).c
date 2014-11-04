fe6192268560f4d6fa2f1fea8af8cc68996606e8
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-11-05 00:44:04 +0300

    Update Task_4 (Sign of number).c

--- 171, 1st semester/Homework_1/Task_4 (Sign of number).c
+++ 171, 1st semester/Homework_1/Task_4 (Sign of number).c
@@ -1,5 +1,9 @@
-//Returns "1" if the number is positive, "-1" if number is negative and "0" if it's zero
-
+/*
+Определяет знак числа. Возвращает "1", если число положительно, "-1" - если отрицательно, 
+и "0", если был введён 0.
+=========================================================================================
+Returns "1" if the number is positive, "-1" if number is negative and "0" if it's zero
+*/
 
 #include <stdio.h>
 
