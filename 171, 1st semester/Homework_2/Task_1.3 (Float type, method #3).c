b8c4981e17a9f63e8db045e053ee50f9f37ad402
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-11-05 00:49:17 +0300

    Update Task_1.3 (Float type, method #3).c

--- 171, 1st semester/Homework_2/Task_1.3 (Float type, method #3).c
+++ 171, 1st semester/Homework_2/Task_1.3 (Float type, method #3).c
@@ -1,5 +1,10 @@
-//Takes as input a float number and divides it on sign, exponent and mantissa using bit field. 
-//Then writes this number in other representation
+/*
+Получает на вход вещественное число и разделяет его на знак, мантиссу и экспоненту, используя битовое поле. 
+Затем выводит это число в другом представлении.
+===========================================================================================================
+Takes as input a float number and divides it on sign, exponent and mantissa using bit field. 
+Then writes this number in other representation
+*/
 
 
 #include <stdio.h>
