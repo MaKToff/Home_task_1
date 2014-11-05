0add918a9eb6248d21730539aa5437377e8254eb
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-11-06 00:16:05 +0300

    Update Task_1.2 (Float type, method #2).c

--- 171, 1st semester/Homework_2/Task_1.2 (Float type, method #2).c
+++ 171, 1st semester/Homework_2/Task_1.2 (Float type, method #2).c
@@ -3,7 +3,9 @@
 Затем выводит это число в другом представлении.
 ====================================================================================================
 Takes as input a float number and divides it on sign, exponent and mantissa using union. 
-Then writes this number in other representation
+Then writes this number in other representation.
+
+Author: Mikhail Kita, group 171
 */
 
 
@@ -18,7 +20,7 @@ struct floatNumber
 
 void print(floatNumber)
 {
-	#define maxNum 255 //It's maximum number for exponent = 2^8 - 1 
+	const int maxNum = 255; //It's maximum number for exponent = 2^8 - 1 
 	if (f.sign) f.sign = -1; else f.sign = 1;
 	if (f.exponent == 0 && f.mantissa == 0) printf("Zero\n");
 	else if (f.exponent == maxNum && f.mantissa == 0) 
