ba144330ba9fd851cfeeb8d969ad7b9b67310353
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-11-06 00:12:58 +0300

    Update Task_4 (Sign of number).c

--- 171, 1st semester/Homework_1/Task_4 (Sign of number).c
+++ 171, 1st semester/Homework_1/Task_4 (Sign of number).c
@@ -1,16 +1,19 @@
 /*
-Определяет знак числа. Возвращает "1", если число положительно, "-1" - если отрицательно, 
-и "0", если был введён 0.
-=========================================================================================
-Returns "1" if the number is positive, "-1" if number is negative and "0" if it's zero
+Определяет знак числа. Возвращает "1", если число положительно, 
+"-1" - если отрицательно, и "0", если был введён 0.
+=================================================================
+Returns "1" if the number is positive, "-1" if number is negative 
+and "0" if it's zero.
+
+Author: Mikhail Kita, group 171
 */
 
 #include <stdio.h>
 
 int sign(int number)
 {
-	const int sh = sizeof(int) * 8 - 1;
-	return (number >> sh) + !(number >> sh) + ~(!number + ~0);
+	const int temp = number >> (sizeof(int) * 8 - 1);
+	return temp + !temp + ~(!number + ~0);
 }
 
 int main(void)
