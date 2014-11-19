c881e4223bca29941a58a7629f1da31f3eb8f9b9
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-11-19 23:53:27 +0300

    Added comments to every function

--- 171, 1st semester/Homework_6/Linked_list.h
+++ 171, 1st semester/Homework_6/Linked_list.h
@@ -17,10 +17,23 @@ typedef struct node
 	struct node *next;
 } node;
 
+//prints message about error
 void error(int value);
+
+//deletes all elements from list
 void del_all(node **head);
+
+//deletes first element with given value
 void del_first(node **head, int data);
+
+//returns size of list
 int size(node **head);
+
+//prints all elements of list
 void print(node **head);
+
+//adds new element at the head of list
 void push_front(node **head, int data);
+
+//adds new element at the end of list
 void push_back(node **head, node **tail, int data);
