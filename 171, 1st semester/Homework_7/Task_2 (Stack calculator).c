a4a6a51e0c30a4c34bd024a7490d4395baa5fe69
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-12-07 19:37:00 +0300

    Fixed all memory leaks

--- 171, 1st semester/Homework_7/Task_2 (Stack calculator).c
+++ 171, 1st semester/Homework_7/Task_2 (Stack calculator).c
@@ -8,118 +8,136 @@ Author: Mikhail Kita, group 171
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "Stack.h"
 
-//deletes all numbers
-void calc_delete_all_numbers(number **value, number **first, number **second, number **result)
-{
-	longNum_delete(value);
-	longNum_delete(first);
-	longNum_delete(second);
-	longNum_delete(result);
-	return;
-}
-
 //processes the commands and calls the necessary functions
-void calc_start(stack_node **stack_head)
+void calc_start(stack_node **stack_head, number **value, number **num1, number **num2, number **result, int *finished)
 {
-	number *value = longNum_init();
-	number *first = longNum_init();
-	number *second = longNum_init();
-	number *result = longNum_init();
-	char space, operation;
-	int i = 0, ok = 1;
+	char trash = '\0', operation = '\0', first_digit = '\0';
+	int i = 0, ok = 0;
 
 	while(1)
 	{	
 		//reading number
-		longNum_clear(&value);
-		longNum_read(&value, &ok);
-		if (!value->sign)
+		longNum_clear(value);
+		operation = '\0';
+		scanf("%c", &first_digit);
+
+		if (first_digit == '#') //user wants to close application
 		{
-			calc_delete_all_numbers(&value, &first, &second, &result);
-			return; //an error occurred
+			*finished = 1;
+			return;
 		}
-		if (!ok)
+
+		else if (first_digit == '+' || first_digit == '*' || first_digit == '/')
 		{
-			calc_delete_all_numbers(&value, &first, &second, &result);
-			exit(0);
+			scanf("%c", &trash);
+			operation = first_digit;
 		}
-		stack_push(stack_head, &value);
+
+		else
+		{
+			longNum_read(value, first_digit, &ok);
+			
+			if ((first_digit < '0' || first_digit > '9') && intList_size(&(*value)->head) == 0)
+			{
+				error(UNKNOWN_COMMAND);
+				printf("== Expected: +, -, * or /. Found: %c\n", first_digit);
+				return;
+			}
+
+			if ((*value)->sign == 0xDEAD)
+			{
+				error(INCORRECT_ARGUMENT);
+				return;
+			}
+
+			if (intList_size(&(*value)->head) == 0)
+			{
+				if (first_digit == '-')
+					operation = first_digit;
+				else
+					return; //an error occurred
+			}
+			else
+				stack_push(stack_head, value);
+		}
 
 		//executes necessary operation
-		if (stack_size(stack_head) == 2)
+		if (operation != '\0')
 		{
-			scanf("%c", &operation);
-			scanf("%c", &space);
-			stack_pop(stack_head, &second);
-			stack_pop(stack_head, &first);
-			result = longNum_init();
+			if (stack_size(stack_head) < 2)
+			{
+				error(STACK_IS_TOO_SMALL);
+				while((int)trash != 10) 
+					scanf("%c", &trash);
+				return;
+			}
+			longNum_delete(num1);
+			longNum_delete(num2);
+			longNum_clear(result);
+
+			stack_pop(stack_head, num2);
+			stack_pop(stack_head, num1);
 
 			//choice of appropriate command
 			if (operation == '+' || operation == '-')
 			{
-				if (first->sign == second->sign)
+				if ((*num1)->sign == (*num2)->sign)
 				{
 					if (operation == '+') 
-						longNum_sum(&first, &second, &result);
+						longNum_sum(num1, num2, result);
 					else 
-						longNum_subtract(&first, &second, &result);
+						longNum_subtract(num1, num2, result);
 				}
 				else 
 				{
 					if (operation == '-') 
-						longNum_sum(&first, &second, &result);
+						longNum_sum(num1, num2, result);
 					else 
-						longNum_subtract(&first, &second, &result);
+						longNum_subtract(num1, num2, result);
 				}
-				result->sign *= first->sign;
+				(*result)->sign *= (*num1)->sign;
 			}
 			else 
 			{
 				if (operation == '*')
-					longNum_multiply(&first, &second, &result);
+					longNum_multiply(num1, num2, result);
 				
 				else if (operation == '/')
 				{
-					if (intList_size(&second->head) == 1 && second->head->value == 0)
+					if (intList_size(&(*num2)->head) == 1 && (*num2)->head->value == 0)
 					{
 						error(DIVISION_BY_ZERO);
-						calc_delete_all_numbers(&value, &first, &second, &result);
 						return;
 					}
-					longNum_divide(&first, &second, &result);
+					longNum_divide(num1, num2, result);
 				}
-				else
-				{
-					error(UNKNOWN_COMMAND);
-					calc_delete_all_numbers(&value, &first, &second, &result);
-					return;
-				}
-				if (first->sign != first->sign)
-					result->sign *= (-1);
-				else result->sign = 1;
+				if ((*num1)->sign != (*num2)->sign)
+					(*result)->sign *= (-1);
+				else (*result)->sign = 1;
 			}
-			result = longNum_reverse(&result);
-			stack_push(stack_head, &result);
+			longNum_reverse(result);
+			stack_push(stack_head, result);
 			
-			//if this is newline
-			if ((int)space == 10)
+			if ((int)trash == 10 || ok) //if newline was introduced
 				break;
 		}
+		if (ok) //the end of input found
+			break;
 	}
 	if (stack_size(stack_head) != 1)
 		error(INCORRECT_EXPRESSION);
 	else
 	{
+		longNum_delete(value);
 		printf("=== ");
-		stack_pop(stack_head, &value);
-		if (value->sign == -1)
+		stack_pop(stack_head, value);
+		if ((*value)->sign == -1)
 			printf("-");
-		intList_print(&longNum_reverse(&value)->head);
+		longNum_reverse(value);
+		intList_print(&(*value)->head);
 	}
-	calc_delete_all_numbers(&value, &first, &second, &result);
 	return;
 }
 
@@ -139,13 +157,34 @@ void calc_help()
 int main(void)
 {
 	stack_node *stack_head = NULL;
+	number *value = longNum_init();
+	number *num1 = longNum_init();
+	number *num2 = longNum_init();
+	number *result = longNum_init();
+	int finished = 0;
+
 	calc_help();
 	while(1)
 	{
 		printf("\n\n\n________________________________\n");
 		printf("Enter the arithmetic expression:\n\n");
-		calc_start(&stack_head);
-		stack_clear_all(&stack_head);
+
+		calc_start(&stack_head, &value, &num1, &num2, &result, &finished);
+
+		if (finished)
+			break;
+
+		//clearing of data
+		longNum_clear(&value);
+		longNum_clear(&result);
+		stack_clear(&stack_head);
 	}
+
+	//deleting all numbers
+	longNum_delete(&value);
+	longNum_delete(&num1);
+	longNum_delete(&num2);
+	longNum_delete(&result);
+	stack_delete(&stack_head);
 	return 0;
 }
