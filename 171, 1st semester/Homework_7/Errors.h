b4dbcf026f3adc82ced00d3fb7c6427adb6f4c47
Mikhail Kita <MaKToff@users.noreply.github.com>
2014-12-07 19:27:54 +0300

    Fixed all memory leaks

--- 171, 1st semester/Homework_7/Errors.h
+++ 171, 1st semester/Homework_7/Errors.h
@@ -14,7 +14,8 @@ enum errors
 	INCORRECT_EXPRESSION = 4,
 	UNKNOWN_COMMAND = 5,
 	STACK_IS_EMPTY = 6,
-	DIVISION_BY_ZERO = 7
+	DIVISION_BY_ZERO = 7,
+	STACK_IS_TOO_SMALL = 8
 } errors;
 
 //prints message about error
