71ea72a9f5950f8742db0621f3b737e6e1253f83
MaKToff <mihail.kita@gmail.com>
2015-05-01 22:29:15 +0300

    Added files of project and README

---
+++ 2nd_semester/Homework_9/src/app/Tasks_46_49__Threads.fs
@@ -0,0 +1,150 @@
+﻿(*
+Homework 9 (27.04.2015)
+Tasks 46 - 49
+
+Author: Mikhail Kita, group 171
+*)
+
+module Program
+
+open System.Threading
+open Calc
+
+//the 46th task
+
+//finds max element in given range
+let maxInRange (arr : int []) left right =
+    let mutable result = arr.[left]
+    for i = left + 1 to right do
+        if arr.[i] > result then result <- arr.[i]
+    result
+
+//finds max element in array
+let maxElem (arr : int []) (threadNumber : int) =
+    let step = arr.Length / threadNumber
+    let result = ref arr.[0]
+    
+    let lock (elem : int ref) value =
+        Monitor.Enter(elem);
+        try
+            if value > elem.Value then elem := value
+        finally
+            Monitor.Exit(elem)
+
+    let threadArray = Array.init threadNumber (fun i ->
+        new Thread(ThreadStart(fun _ ->
+            let threadRes = maxInRange arr (i * step) ((i + 1) * step - 1)
+            lock result threadRes
+          ))
+      )
+
+    for t in threadArray do t.Start()
+    for t in threadArray do t.Join()
+    
+    if (threadNumber * step - 1) < arr.Length - 1 then
+        lock result (maxInRange arr (threadNumber * step) (arr.Length - 1))
+    result.Value
+
+
+//the 47th task
+
+//finds value of defined integral from left to right border
+let squareInRange expression left right precision =
+    let mutable result = 0.0
+    let mutable i = left
+    let context = new Dictionary()
+    
+    let computeWithContext value =
+        context.add "x" value
+        compute expression context
+
+    while i < right do
+        let a = computeWithContext i
+        let b = computeWithContext (i + precision)
+        result <- result + ((a + b) / 2.0 * precision)
+        i <- i + precision
+    result
+
+//finds value of defined integral on the whole interval
+let definiteIntegral expression (threadNumber : int) left right precision =
+    let n = right - left
+    let step = n / (float threadNumber)
+    let result = ref 0.0
+
+    let lock (elem : float ref) value =
+        Monitor.Enter(elem);
+        try
+            elem := elem.Value + value
+        finally
+            Monitor.Exit(elem)
+
+    let l = ref left
+    let r = ref left
+    let threadArray = Array.init threadNumber (fun i ->
+        new Thread(ThreadStart(fun _ ->
+            l := r.Value
+            r := r.Value + step
+            let threadRes = squareInRange expression l.Value r.Value precision
+            lock result threadRes
+          ))
+      )
+    
+    for t in threadArray do t.Start()
+    for t in threadArray do t.Join()
+    result.Value
+
+
+//the 49th task
+
+//sorts given interval in array by merging
+let sortInRange (arr : int []) start1 start2 n =
+    let mutable p1  = start1
+    let mutable p2  = start2
+    let mutable res = []
+
+    for i = 0 to n - 1 do
+        if p2 < start1 + n then
+            if arr.[p1] < arr.[p2] then
+                if p1 < start2 then
+                    res <- List.append res [arr.[p1]]
+                    p1 <- p1 + 1
+                else
+                    res <- List.append res [arr.[p2]]
+                    p2 <- p2 + 1
+            else
+                res <- List.append res [arr.[p2]]
+                p2 <- p2 + 1
+        else
+            res <- List.append res [arr.[p1]]
+            p1 <- p1 + 1
+    
+    for i = 0 to res.Length - 1 do
+        arr.[i + start1] <- res.[i]
+
+//sorts array in ascending order
+let mergeSort (arr : int []) =
+    let n    = arr.Length
+    let step = ref 2
+    
+    while step.Value < n do
+        let start = ref -step.Value
+        let threadArray = Array.init (n / step.Value) (fun i ->
+            new Thread(ThreadStart(fun _ ->
+                start := start.Value + step.Value
+                sortInRange arr start.Value (start.Value + step.Value / 2) step.Value
+            ))
+        )
+    
+        for t in threadArray do t.Start()
+        for t in threadArray do t.Join()
+        step := step.Value * 2
+    
+    step := step.Value / 2
+    if (step.Value) < n then 
+        sortInRange arr 0 step.Value n
+    arr
+
+
+[<EntryPoint>]
+let main argv =
+    0
---
+++ 2nd_semester/Homework_9/src/test/Tests.fs
@@ -0,0 +1,58 @@
+﻿(*
+Homework 9 (27.04.2015)
+Tests for tasks 46 - 49
+
+Author: Mikhail Kita, group 171
+*)
+
+module Tests
+
+open NUnit.Framework
+open Program
+
+//tests for 46th task
+[<TestCase ([|7|], 7, 1)>]
+[<TestCase ([|1; -2; 3; -4; 9; -5; 6; -7; 8; 0|], 9, 10)>]
+[<TestCase ([|-1; -2; -3; -4; -5; -6; -7; -8; -9|], -1, 9)>]
+let ``Test for task #46 01`` (arr : int []) value n =
+    for i = 1 to n do
+        Assert.AreEqual(value, maxElem arr i)
+
+[<Test>]
+let ``Test for task #46 02`` () =
+    let arr = Array.init 100 (fun i -> 1)
+    for i = 1 to 100 do
+        Assert.AreEqual(1, maxElem arr i)
+
+[<Test>]
+let ``Test for task #46 03`` () =
+    let n = 100000
+    let arr = Array.init n (fun i -> System.Random(0).Next(0, n))
+    for i = 1 to 100 do
+        Assert.AreEqual(72624, maxElem arr i)
+
+
+//tests for 47th task
+[<TestCase ("x", -5.0, 7.0, 0.0001, 2, 12.0)>]
+[<TestCase ("x", -5.0, 5.0, 0.0001, 3, 0.0)>]
+[<TestCase ("5 * ln x", 1.0, 5.0, 0.0001, 2, 20.24)>]
+[<TestCase ("x ^ 2 + 3", 0.0, 3.0, 0.0001, 2, 18.0)>]
+[<TestCase ("sin x + cos x", 0.0, System.Math.PI, 0.0001, 3, 2.0)>]
+let ``Test for task #47 01`` expression left right precision (digits : int) answer =
+    for i = 1 to 10 do
+        let result = definiteIntegral expression i left right precision
+        Assert.AreEqual(answer, (System.Math.Round(result, digits)))
+
+
+//tests for 49th task
+[<TestCase ([|3|], Result = [|3|])>]
+[<TestCase ([|1; 2; 3|], Result = [|1; 2; 3|])>]
+[<TestCase ([|-1; 4; -17; 3; 8|], Result = [|-17; -1; 3; 4; 8;|])>]
+[<TestCase ([|9; 2; 3; 8; 5; 7; 4; 6; 1|], Result = [|1; 2; 3; 4; 5; 6; 7; 8; 9|])>]
+let ``Test for task #49 01`` (arr : int []) =
+    mergeSort arr
+
+[<Test>]
+let ``Test for task #49 02`` () =
+    let arr = Array.init 1000 (fun i -> i)
+    Assert.AreEqual(arr, mergeSort arr)
