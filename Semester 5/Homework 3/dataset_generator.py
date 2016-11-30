209e47a9ddabd30e914f18f8aed6c02f3c5ca723
Mikhail Kita <mihail.kita@gmail.com>
2016-11-30 23:38:35 +0300

    Updated homework 3

--- Semester 5/Homework 3/dataset_generator.py
+++ Semester 5/Homework 3/dataset_generator.py
@@ -8,8 +8,12 @@ Author: Mikhail Kita, group 371
 import os
 import cv2
 import numpy as np
+from math import sqrt, pi, sin, cos
 from random import randint
-from common import *
+
+squares_path = "./data/squares/"
+triangles_path = "./data/triangles/"
+tests_path = "./data/tests/"
 
 if not os.path.exists(squares_path):
     os.makedirs(squares_path)
@@ -25,11 +29,18 @@ if not os.path.exists(tests_path):
 def generate_square(filename, side):
     image = np.ones((side, side, 3)) * 255
 
-    edge = randint(int(side * 0.1), side)
-    x = randint(0, side - edge)
-    y = randint(0, side - edge)
+    edge = randint(int(side * 0.2), int(side / 2.0 * sqrt(2.0)))
+    temp = int(edge / sqrt(2.0))
+    x0 = randint(temp, side - temp)
+    y0 = randint(temp, side - temp)
+    a = randint(0, 90) * pi / 180.0
+    t = int(edge / 2.0)
 
-    square = np.array([[x, y], [x + edge, y], [x + edge, y + edge], [x, y + edge]])
+    def turn(x, y):
+        return [x0 + int(x * cos(a) - y * sin(a)),
+                y0 + int(x * sin(a) + y * cos(a))]
+
+    square = np.array([turn(-t, -t), turn(t, -t), turn(t, t), turn(-t, t)])
     color = (randint(0, 255), randint(0, 255), randint(0, 255))
 
     cv2.fillPoly(image, [square], color)
@@ -55,9 +66,9 @@ def generate_triangle(filename, side):
 
 
 for i in range(0, 800):
-    generate_square(squares_path + "square-" + str(i) + ".png", 50)
-    generate_triangle(triangles_path + "triangle-" + str(i) + ".png", 50)
+    generate_square(squares_path + "square-" + str(i) + ".bmp", 50)
+    generate_triangle(triangles_path + "triangle-" + str(i) + ".bmp", 50)
 
 for i in range(0, 200):
-    generate_square(tests_path + "square-" + str(i) + ".png", 50)
-    generate_triangle(tests_path + "triangle-" + str(i) + ".png", 50)
+    generate_square(tests_path + "square-" + str(i) + ".bmp", 50)
+    generate_triangle(tests_path + "triangle-" + str(i) + ".bmp", 50)
--- Semester 5/Homework 3/testing.py
+++ Semester 5/Homework 3/testing.py
@@ -5,31 +5,36 @@ Performs recognition of test images.
 Author: Mikhail Kita, group 371
 """
 
+import os
 import re
 import cv2
-import numpy as np
-from sklearn.externals import joblib
-from common import *
 
-tests, labels = joblib.load("lbp.pkl")
-images = get_images(tests_path)
+
+# Gets all images located in given directory
+def get_images(path):
+    result = []
+
+    for current_path, dirs, files in os.walk(path):
+        for file in files:
+            image = cv2.imread(current_path + "/" + file)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            result.append({"filename": file, "image": gray})
+
+    return result
+
+
 ok = 0
+cascade = cv2.CascadeClassifier("cascade.xml")
+images = get_images("./data/tests")
 
-for file in images:
-    hist = calc_hist(file[1])
-    results = []
+for img in images:
+    squares = cascade.detectMultiScale(img["image"], 1.3, 5)
+    answer = re.sub(r"(.*)-.*", r"\1", img["filename"])
 
-    for index, x in enumerate(tests):
-        score = cv2.compareHist(np.array(x, dtype=np.float32), np.array(hist, dtype=np.float32), 1)
-        results.append((labels[index], score))
-
-    results = sorted(results, key=lambda t: t[1])
-    answer = re.sub(r"(.*)-.*", r"\1", file[0])
-
-    if answer == results[0][0]:
+    if ((len(squares) == 0) & (answer == "triangle")) | ((len(squares) > 0) & (answer == "square")):
         ok += 1
         print("OK")
     else:
-        print("Incorrect, image {} is not a {}.".format(file[0], results[0][0]))
+        print("Image {} recognized incorrectly.".format(img["filename"]))
 
 print("Accuracy: ", str(round(float(ok) / float(len(images)) * 100, 2)) + "%")
